f11908ebc805e3d4db48270fad8839293c8558a7
Willians Martins <professorwillians@gmail.com>
2020-04-19 01:16:49 -0300

    aula6

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -2,6 +2,7 @@ package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -16,12 +17,28 @@ public class RotaPedidos {
 
 			@Override
 			public void configure() throws Exception {
+				
+				errorHandler(deadLetterChannel("file:erro").
+						logExhaustedMessageHistory(true).
+							maximumRedeliveries(3).
+								redeliveryDelay(2000).
+									onRedelivery(new Processor() {
+										
+										@Override
+										public void process(Exchange exchange) throws Exception {
+											int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+										    int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+											System.out.println("Redelivery " + counter + "/" + max);
+										}
+									})
+								);
 
 				from("file:pedidos?delay=5s&noop=true").
 					routeId("rota-pedidos").
-					multicast().
-						to("direct:soap").
-						to("direct:http");
+					to("validator:pedido.xsd");
+//					multicast().
+//						to("direct:soap").
+//						to("direct:http");
 				
 				from("direct:http").
 					routeId("rota-http").
@@ -33,15 +50,17 @@ public class RotaPedidos {
 						xpath("/item/formato[text()='EBOOK']").
 					setProperty("ebookId", xpath("/item/livro/codigo/text()")).
 					marshal().xmljson().
-					log("${id} - ${body}").
+					//log("${id} - ${body}").
 					setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
 					setHeader(Exchange.HTTP_QUERY,simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).
 				to("http4://localhost:8080/webservices/ebook/item");
 				
 				from("direct:soap").
 					routeId("rota-soap").
-					setBody(constant("<envelope>teste</envelope>")).
-				to("mock:soap");
+					to("xslt:pedido-para-soap.xslt").
+					log("${body}").
+					setHeader(Exchange.CONTENT_TYPE,constant("text/xml")).
+				to("http4://localhost:8080/webservices/financeiro");
 			}
 			
 		});
